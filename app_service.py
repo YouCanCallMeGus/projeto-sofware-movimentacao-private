5e800dee7bcdd65c082e0e4761be67d0cceeaaeb
YouCanCallMeGus <gustavosantanas.2019@gmail.com>
2025-10-01 10:26:16 -0300

    run

--- app_service.py
+++ app_service.py
@@ -1,4 +1,10 @@
 import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+URL = os.getenv("URL")
+
 
 def validar_campos(data):
     campos_obrigatorios = ["cpf_comprador", "cpf_vendedor", "ticker", "quantidade"]
@@ -8,7 +14,7 @@ def validar_campos(data):
     return None
 
 def calcular_valor(ticker, quantidade):
-    url = f"http://stocks:8080/stocks/{ticker}"
+    url = f"{URL}/stocks/{ticker}"
     try:
         resp = requests.get(url, timeout=5)
         if resp.status_code != 200:
